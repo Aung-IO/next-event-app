f58f094d1b20d4befa3aaf3f864b9e26c6cd1b3c
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2024-04-20 01:45:16 +0630

    get related events

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -4,6 +4,7 @@ import {
   CreateEventParams,
   DeleteEventParams,
   GetAllEventsParams,
+  GetRelatedEventsByCategoryParams,
   UpdateEventParams,
 } from "@/types";
 import { revalidatePath } from "next/cache";
@@ -108,6 +109,35 @@ export const updateEvent = async ({
     revalidatePath(path);
 
     return JSON.parse(JSON.stringify(updatedEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getRelatedEventsByCategory = async ({
+  eventId,
+  categoryId,
+  limit = 3,
+  page = 1,
+}: GetRelatedEventsByCategoryParams) => {
+  try {
+    await connectToDatabase();
+    const skipAmount = (Number(page) - 1) * limit;
+    const conditions = {
+      $and: [{ category: categoryId }, { _id: { $ne: eventId } }],
+    };
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (error) {
     handleError(error);
   }
